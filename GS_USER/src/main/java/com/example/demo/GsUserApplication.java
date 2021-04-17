2865b2c15807cc1b62539f314a8f07da4fe1d289
RayMrsite <raymrsitecode@gmail.com>
2021-04-17 12:42:00 -0500

    Intento numero dos de carga de archivo

---
+++ GS_USER/src/main/java/com/example/demo/GsUserApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsUserApplication.class, args);
+	}
+
+	
+}
